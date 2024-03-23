37dfe967507b88d82682641c43247accce28ab21
Dylan Stocking <dstocking0823@gmail.com>
2024-03-23 16:37:22 -0400

    Finish Step 1.5

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
+import Guess from '../Guess';
+
 function PreviousGuesses({ guesses }) {
     return (
       <div className="guess-results">
-        {guesses.map((guess, index) => (
-          <p key={index} className="guess">
-            {guess}
-          </p>
+        {range(NUM_OF_GUESSES_ALLOWED).map((guessIndex) => (
+            <Guess key={guessIndex} value={guesses[guessIndex]} />   
         ))}
       </div>
     );
